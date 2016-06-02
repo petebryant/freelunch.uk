f013ed856ec06eac51232ce7cb7fef7f626364ea
peterbryant <pete.bryant@gmail.com>
2016-06-02 08:38:06 +0100

    cleanup login and user modal

---
+++ freelunch.uk/Common/Functions.cs
@@ -0,0 +1,33 @@
+﻿using freelunch.uk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using freelunch.uk.Models;
+
+namespace freelunch.uk.Common
+{
+    public class Functions
+    {
+        public static bool IsExpert(string userId)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var expert = context.Experts.FirstOrDefault(x => x.UserId == userId);
+
+            return (expert != null);
+        }
+    }
+}
